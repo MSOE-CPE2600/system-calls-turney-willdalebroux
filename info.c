be5620aa16ddf0c1638cb975d0bc17d434795942
Will Dalebroux <dalebrouxw@msoe.edu>
2025-10-30 16:32:51 -0500

    print hostname

--- info.c
+++ info.c
@@ -2,8 +2,33 @@
 
 // info.c
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+
 int main(int argc, char* argv[])
 {
+    // struct timespec ts;
+    // clock_gettime(CLOCK_REALTIME, &ts);
 
-    return 0;
+    // printf("seconds: %ld\n", ts.tv_sec);
+    char hostname[256]; // declare array with reasonable size for host name
+    int result; // declare result for error handling
+
+    //call gethostname() to retrieve host name
+    result = gethostname(hostname, sizeof(hostname));
+    if(result == 0)
+    {
+        //success
+        printf("Hostname is: %s\n", hostname);
+    }
+    else
+    {
+        //fail
+        printf("Error: hostname not found\n");
+    }
+    
+
 }
