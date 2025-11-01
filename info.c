95161b9ed0399a44dcc52a661b106e9d882c7f8c
Will Dalebroux <dalebrouxw@msoe.edu>
2025-11-01 16:54:50 -0500

    added error handling

--- info.c
+++ info.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/utsname.h>
 #include <sys/sysinfo.h>
 
@@ -31,7 +32,7 @@ int main(int argc, char* arg[])
     else
     {
         //fail
-        printf("Error: hostname not found\n");
+        perror("Error: hostname not found");
     }
 
     struct utsname name; // declare struct to store system information in
@@ -47,23 +48,54 @@ int main(int argc, char* arg[])
     else 
     {
         //fail
-        printf("Error: sysname not found\n");
+        perror("uname");
     }
 
     //get # of processors
     int processors = get_nprocs_conf(); 
-    printf("number of processors is: %d\n", processors);
+    if(processors > 0)
+    {
+        printf("number of processors is: %d\n", processors);
+    }
+    else 
+    {
+        perror("no processors found");
+    }
+    
 
     //total ammount of physical memory
+    errno = 0; // error handling
     long total_pages = sysconf(_SC_PHYS_PAGES); // 8 billion will not fit in an int so long was used
+    if(total_pages == -1 && errno != 0)
+    {
+        perror("sysconf(_SC_PHYS_PAGES)");
+    }
+
+    errno = 0; // clear errno to prevent wrong error from being thrown
     long page_size = sysconf(_SC_PAGESIZE); // size of page in bytes
+    if(page_size == -1 && errno != 0)
+    {
+        perror("sysconf(_SC_PAGESIZE)");
+    }
 
     long total_mem = total_pages * page_size;
-
-    long free_mem = sysconf( _SC_AVPHYS_PAGES) * page_size;
-
+    //note: WSL only has 8gb RAM, despite thinkpad x1 having 16gb RAM
     printf("total physical memory is %lu bytes\n (%.2f Gb)\n", total_mem, total_mem / 1e9);
-    printf("free memory is: %ld bytes\n(%.2f Gb)\n", free_mem, free_mem / 1e9);
+
+    // long free_mem = sysconf( _SC_AVPHYS_PAGES) * page_size; // multiply free
+    errno = 0;
+    if(sysconf(_SC_AVPHYS_PAGES) > 0)
+    {
+        long free_mem = sysconf(_SC_AVPHYS_PAGES) * page_size;
+        printf("free memory is: %ld bytes\n(%.2f Gb)\n", free_mem, free_mem / 1e9);
+    }
+    else
+    {
+        perror("_SC_AVPHYS_PAGES");
+    }
+
+    
+   
     
 
 }
